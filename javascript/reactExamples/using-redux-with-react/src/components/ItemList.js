cf40866d3424d6e482baae6905b147f4b120d85e
Brad Wight <bwight@scentsy.com>
2018-11-26 09:41:30 -0700

    Adding react examples

---
+++ javascript/reactExamples/using-redux-with-react/src/components/ItemList.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { itemsFetchData } from '../actions/items';
+
+class ItemList extends Component {
+    componentDidMount() {
+        this.props.fetchData('http://api.tvmaze.com/shows');
+    }
+
+    render() {
+
+        if (this.props.hasError) {
+            return <p>Sorry! There was an error loading the items</p>;
+        }
+
+        if (this.props.isLoading) {
+            return <p>Loading…</p>;
+        }
+
+        return(
+            <div>
+                {this.props.items.map((item) => (
+                    <div key={item.id}>
+                        <p>{item.name} : {item.rating.average}</p>
+                    </div>
+                ))}
+            </div>
+        );
+        
+    }  
+}
+
+const mapStateToProps = (state) => {
+    return {
+        items: state.items,
+        hasError: state.itemsHaveError,
+        isLoading: state.itemsAreLoading
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchData: (url) => dispatch(itemsFetchData(url))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemList);
